a04e5f7461fd659682c1ec07bbb623501c0cd4ce
desolation-dev <sam.joe.trit@gmail.com>
2024-05-12 14:32:27 +1200

    Implemented requirement 1 (brew-coffee endpoint)

--- CoffeeApi/Program.cs
+++ CoffeeApi/Program.cs
@@ -1,6 +1,22 @@
+using System.Globalization;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Setup the endpoints
+app.MapGet("/brew-coffee", BrewCoffee);
 
+// Run the application
 app.Run();
+
+
+// Public partial class to allow easy access to the public methods for unit tests
+public static partial class Program
+{
+    // Implementaion for the brew-coffee endpoint
+    public static IResult BrewCoffee() => Results.Json(new {
+        message = "Your piping hot coffee is ready",
+        // ISO-8601 formated dateTime
+        prepared = DateTimeOffset.Now.ToString("o", CultureInfo.InvariantCulture)
+    });
+}
