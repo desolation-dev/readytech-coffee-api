b344daba2c3cefc4cadb13f47c6d409a44e3d91b
desolation-dev <sam.joe.trit@gmail.com>
2024-05-13 15:06:02 +1200

    Implemented requirement 2 (service unavailable on fifth call); added/updated unit tests

--- CoffeeApi.Tests/ProgramTests.cs
+++ CoffeeApi.Tests/ProgramTests.cs
@@ -1,16 +1,52 @@
+using Microsoft.AspNetCore.Http.Connections;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 namespace CoffeeApi.Tests;
 
 public class ProgramTests
 {
-    [Fact]
-    public void BrewCoffee_ReturnsJsonOfBrewCoffeeResponseResult()
+    [Theory]
+    [InlineData(0)]
+    [InlineData(1)]
+    [InlineData(2)]
+    [InlineData(3)]
+    [InlineData(5)]
+    [InlineData(20)]
+    public void BrewCoffee_ReturnsJsonOfBrewCoffeeResponseResult(int numCalls)
     {
+        // Arange
+        int calls = numCalls;
+        
         // Act
-        var actual = Program.BrewCoffee();
+        var result = Program.BrewCoffee(ref calls).Result as JsonHttpResult<Program.BrewCoffeeResponse>;
 
         // Assert
-        Assert.IsType<JsonHttpResult<Program.BrewCoffeeResponse>>(actual);
+        Assert.NotNull(result);
+        
+        Assert.Equal(200, result.StatusCode);
+
+        Assert.NotNull(result.Value);
+        Assert.IsType<Program.BrewCoffeeResponse>(result.Value);
+
+        Assert.Equal(numCalls + 1, calls);
+    }
+
+    [Theory]
+    [InlineData(4)]
+    [InlineData(9)]
+    [InlineData(49)]
+    public void BrewCoffee_ReturnsStatusCode503Result(int numCalls)
+    {
+        // Arrange
+        int calls = numCalls;
+
+        // Act
+        var actual = Program.BrewCoffee(ref calls).Result as StatusCodeHttpResult;
+
+        // Assert
+        Assert.NotNull(actual);
+        Assert.Equal(503, actual.StatusCode);
+
+        Assert.Equal(numCalls + 1, calls);
     }
 }
--- CoffeeApi/Program.cs
+++ CoffeeApi/Program.cs
@@ -1,30 +1,53 @@
 using System.Globalization;
+using Microsoft.AspNetCore.Http.HttpResults;
 
 public class Program
 {
+    // SESSION DATA //
+    /// <summary>
+    /// The number of times the brew-coffee endpoint has been called
+    /// </summary>
+    static int brewCoffeeCalls = 0;
+
+    // MAIN //
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
 
         // Setup the endpoints
-        app.MapGet("/brew-coffee", BrewCoffee);
+        app.MapGet("/brew-coffee", () => BrewCoffee(ref brewCoffeeCalls));
 
         // Run the application
         app.Run();
     }
 
+
     /// <summary>
     /// Implementation of the brew-coffee endpoint. Should return a 200 OK status code
-    /// with a BrewCoffeeResponse in the respose body serialized as a JSON object.
+    /// with a BrewCoffeeResponse in the respose body serialized as a JSON object. On 
+    /// every fifth call, should return a status code 503 empty response instead.
     /// </summary>
-    public static IResult BrewCoffee () => Results.Json(
-        new BrewCoffeeResponse {
-            Message = "Your piping hot coffee is ready",
-            // ISO-8601 formated dateTime
-            Prepared = DateTimeOffset.Now.ToString("o", CultureInfo.InvariantCulture)
+    public static Results<JsonHttpResult<BrewCoffeeResponse>, StatusCodeHttpResult> BrewCoffee(ref int endpointCalls) {
+        // Update the amount of times this endpoint has been called
+        endpointCalls++;
+        // Return status code 503 if out of coffee (every fifth response)
+        if (endpointCalls % 5 == 0)
+        {
+            return TypedResults.StatusCode(503);
         }
-    );
+        // Return standard response
+        else
+        {
+            return TypedResults.Json(
+                new BrewCoffeeResponse {
+                    Message = "Your piping hot coffee is ready",
+                    // ISO-8601 formated dateTime
+                    Prepared = DateTimeOffset.Now.ToString("o", CultureInfo.InvariantCulture)
+                }, statusCode: 200
+            );
+        }
+    }
 
 
     /// <summary>
