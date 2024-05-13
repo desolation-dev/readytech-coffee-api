57de21fb8d16200d3d6739ecd5227023d25b9237
desolation-dev <sam.joe.trit@gmail.com>
2024-05-13 20:39:37 +1200

    Implemented extra credit requirement

--- CoffeeApi/Program.cs
+++ CoffeeApi/Program.cs
@@ -1,20 +1,36 @@
 using System.Globalization;
+using System.Net.Http.Headers;
+using System.Text.Json.Nodes;
 using Microsoft.AspNetCore.Http.HttpResults;
 
 public class Program
 {
+    private const string OPEN_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather";
+    private const string API_KEY = "a5d15c3dc316c9a6f5a802455899d143";
+    private const string LAT = "-37.783333";
+    private const string LON = "175.283333";
+    private static string urlParameters = "?appid="+API_KEY+"&lat="+LAT+"&lon="+LON+"&units=metric";
+
     // SESSION DATA //
     /// <summary>
     /// The number of times the brew-coffee endpoint has been called
     /// </summary>
     static int brewCoffeeCalls = 0;
 
+    public static HttpClient openWeatherHttpClient = new()
+    {
+        BaseAddress = new Uri(OPEN_WEATHER_URL)
+    };
 
     // MAIN //
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
         var app = builder.Build();
+
+        // Setup the openApiHttpClient
+        openWeatherHttpClient.DefaultRequestHeaders.Accept.Add(
+            new MediaTypeWithQualityHeaderValue("application/json"));
 
         // Setup the endpoints
         app.MapGet("/brew-coffee", () => BrewCoffee(ref brewCoffeeCalls));
@@ -42,9 +58,35 @@ public class Program
         // Return standard response
         else
         {
+            // Default message
+            string message = "Your piping hot coffee is ready";
+            // Get the current temperature by querying the openWeather api
+            HttpResponseMessage response = openWeatherHttpClient.GetAsync(urlParameters).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                // Parse the returned json content
+                var parsedResponse = JsonObject.Parse(response.Content.ReadAsStringAsync().Result);
+                var jsonNode = parsedResponse?["main"]?["temp"];
+                if (jsonNode != null)
+                {
+                    double temp = jsonNode.GetValue<double>();
+                    if (temp > 30) message = "Your refreshing iced coffee is ready";
+                }
+                else
+                {
+                    Console.WriteLine("Failed to get current temperature");
+                }
+            }
+            else
+            {
+                // Failed to get the current weather. Log error
+                Console.WriteLine("Failed to get current weather");
+                Console.WriteLine("{0} ({1})", (int)response.StatusCode, response.ReasonPhrase);
+            }
+            // Finally return the brew coffee response
             return TypedResults.Json(
                 new BrewCoffeeResponse {
-                    Message = "Your piping hot coffee is ready",
+                    Message = message,
                     // ISO-8601 formated dateTime
                     Prepared = DateTimeOffset.Now.ToString("o", CultureInfo.InvariantCulture)
                 }, statusCode: 200
