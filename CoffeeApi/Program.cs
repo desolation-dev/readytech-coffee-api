883ff9625767f762e98de54f08e73855269dd984
desolation-dev <sam.joe.trit@gmail.com>
2024-05-13 13:16:53 +1200

    Refactored to use a response object to allow for easier testing

--- CoffeeApi/Program.cs
+++ CoffeeApi/Program.cs
@@ -1,22 +1,39 @@
 using System.Globalization;
 
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-// Setup the endpoints
-app.MapGet("/brew-coffee", BrewCoffee);
-
-// Run the application
-app.Run();
-
-
-// Public partial class to allow easy access to the public methods for unit tests
-public static partial class Program
+public class Program
 {
-    // Implementaion for the brew-coffee endpoint
-    public static IResult BrewCoffee() => Results.Json(new {
-        message = "Your piping hot coffee is ready",
-        // ISO-8601 formated dateTime
-        prepared = DateTimeOffset.Now.ToString("o", CultureInfo.InvariantCulture)
-    });
+    private static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        // Setup the endpoints
+        app.MapGet("/brew-coffee", BrewCoffee);
+
+        // Run the application
+        app.Run();
+    }
+
+    /// <summary>
+    /// Implementation of the brew-coffee endpoint. Should return a 200 OK status code
+    /// with a BrewCoffeeResponse in the respose body serialized as a JSON object.
+    /// </summary>
+    public static IResult BrewCoffee () => Results.Json(
+        new BrewCoffeeResponse {
+            Message = "Your piping hot coffee is ready",
+            // ISO-8601 formated dateTime
+            Prepared = DateTimeOffset.Now.ToString("o", CultureInfo.InvariantCulture)
+        }
+    );
+
+
+    /// <summary>
+    /// The response object to return when calling the brew-coffee endpoint
+    /// </summary>
+    public class BrewCoffeeResponse
+    {
+        public required string Message { get; set; }
+        public required string Prepared { get; set; }
+    }
+
 }
